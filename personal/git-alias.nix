6a6d0579a5a11e861905821ee75d07927b64d6c8
aster <137767097+aster-void@users.noreply.github.com>
2024-04-21 17:17:54 +0900

    changed git config

--- personal/git-alias.nix
+++ personal/git-alias.nix
@@ -1,21 +1,22 @@
 {
   programs.git.config.alias = {
-        a = "add";
-	aa = "add -A";
-        c = "commit";
-        cm = "commit -m";
-	cma = "add -A ; git commit -m ";
-        co = "checkout";
-        b = "branch";
-	ba = "branch -a";
-        s = "status";
-	f = "fetch --prune";
-	upstream = "push --set-upstram origin";
-        unstage = "reset HEAD --";
-        last = "log -1 HEAD";
-        discard = "reset --hard HEAD";
-	uncommit = "reset HEAD~";
-	amend = "commit --amend --no-edit";
-	recommit = "commit --amend";
-      };
+      a = "add";
+	    aa = "add -A";
+      c = "commit";
+      cm = "commit -m";
+      co = "checkout";
+      sw = "switch";
+      b = "branch";
+	    ba = "branch -a";
+      bd = "branch -d";
+      s = "status";
+      f = "fetch --prune";
+	    u = "push --set-upstram origin";
+      unstage = "reset HEAD --";
+      last = "log -1 HEAD";
+      discard = "reset --hard HEAD";
+	    uncommit = "reset HEAD~";
+	    amend = "commit --amend --no-edit";
+	    recommit = "commit --amend";
+    };
 }
