4bccd6b730117c4255a04869e622829f973ed832
aster <137767097+aster-void@users.noreply.github.com>
2024-12-18 14:58:04 +0900

    install fcitx + mozc even when inputMethod is not enabled

--- desktop/plasma6.nix
+++ desktop/plasma6.nix
@@ -1,9 +1,12 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   imports = [
     ./wayland.nix
   ];
-  services.desktopManager.plasma6 = {
-    enable = true;
-  };
+  # enabled at home-manager side
+  # services.desktopManager.plasma6 = {
+    # enable = true;
+  # };
+  environment.systemPackages = with pkgs; [ fcitx5 ];
+  i18n.inputMethod.enable = lib.mkForce false;
 }
--- packages/ime.nix
+++ packages/ime.nix
@@ -1,14 +1,17 @@
 { pkgs, ... }:
+let fcitx5-addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+      libsForQt5.fcitx5-qt
+    ];
+in
 {
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc # todo: uncomment this s.t. I can type Japanese
-      fcitx5-gtk
-      libsForQt5.fcitx5-qt
-    ];
+    fcitx5.addons = fcitx5-addons;
   };
+  environment.systemPackages = fcitx5-addons; # s.t. it works when inputMethod is disabled (such as on Plasma)
 
   environment.variables = {
     # fcitx says they are recommended to be unset
