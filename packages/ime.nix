82b5ed10783dd77463dc708634f66b826f6cbccc
aster <137767097+aster-void@users.noreply.github.com>
2024-12-21 18:28:32 +0900

    edit ime

--- packages/ime.nix
+++ packages/ime.nix
@@ -1,5 +1,7 @@
 { pkgs, ... }:
-let fcitx5-addons = with pkgs; [
+let 
+enable = true;
+fcitx5-addons = with pkgs; [
       fcitx5-mozc
       fcitx5-gtk
       libsForQt5.fcitx5-qt
@@ -7,19 +9,14 @@ let fcitx5-addons = with pkgs; [
 in
 {
   i18n.inputMethod = {
-    enable = true;
+    inherit enable;
     type = "fcitx5";
     fcitx5.addons = fcitx5-addons;
   };
-  environment.systemPackages = fcitx5-addons; # s.t. it works when inputMethod is disabled (such as on Plasma)
+  environment.systemPackages = if enable then [] else fcitx5-addons; # s.t. it works when inputMethod is disabled (such as on Plasma)
 
   environment.variables = {
-    # fcitx says they are recommended to be unset
-    # GTK_IM_MODULE = "wayland";
-    # QT_IM_MODULE = "fcitx";
     XMODIFIERS = "@im=fcitx";
-    SDL_IM_MODULE = "fcitx";
-    # GLFW_IM_MODULE = "ibus";
   };
   services.xserver.desktopManager.runXdgAutostartIfNone = true;
 }
