92fa32c44856aae3607ff2b35effec9cd472c5fc
Yuki Kobayashi <137767097+aster-void@users.noreply.github.com>
2024-12-22 15:01:32 +0900

    Create GitHub action to validate it (#3)

--- samples/password.nix
+++ samples/password.nix
@@ -1,3 +1,13 @@
 {
-  users.users.aster.hashedPassword = "YOUR_HASHED_PASSWORD_USE_mkpasswd_TO_CREATE_YOURS";
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.user = "aster";
+  services.displayManager.autoLogin.enable = true;
+
+  # @CHANGEME: YOU MUST CHANGE THIS!!!
+  # this is a bcrypt of password "password",
+  # in case you accidentally rebuilded it without changing.
+  users.users.aster.hashedPassword =
+    "$y$j9T$R95R3yL52VbyQHOsaLXvy.$Rpm7Iwgs2SSIGmsiQDtRbTwZnXT5q1OoL9uAfogVH04";
 }
+
+
--- user.nix
+++ user.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }:
 {
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.user = "aster";
-  services.displayManager.autoLogin.enable = false;
-
   # Define a user account.
   users.users.aster = {
     isNormalUser = true;
