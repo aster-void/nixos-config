de88bc3a2cac5f9340cfc684386613ff0a952d2d
aster <137767097+aster-void@users.noreply.github.com>
2024-12-22 10:52:53 +0900

    comment out wsl configuration by default

--- samples/optional.nix
+++ samples/optional.nix
@@ -5,7 +5,7 @@
     ./optional/hardware # comment this out if you are installing this on WSL
     # ./optional/drivers/logitech.nix
     # ./optional/drivers/nvidia.nix
-    ./optional/wsl/configuration.nix
+    # ./optional/wsl/configuration.nix
   ];
 
   boot.loader.grub.device = "nodev"; # or to use an actual device, use /dev/disk/by-uuid/YOUR_DEVICE_UUID_USE_ls_-lha_TO_CHECK
