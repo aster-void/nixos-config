b98af421da3f2f13fc7e6c7af856869a2dede14a
aster <137767097+aster-void@users.noreply.github.com>
2024-12-23 17:44:22 +0900

    fix samples/ and init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
+set -euo pipefail
 cd `dirname -- $0`
 
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 
 if [ ! -f ./local/hardware-configuration.nix ]; then
-  echo 'Generating hardware-configuration.nix...'
-  TMP_DIR="./tmp"
-  mkdir -p ${TMP_DIR}
-  nixos-generate-config --dir ${TMP_DIR}
-  mv ./tmp/hardware-configuration.nix
-  rm ${TMP_DIR} -r
+  if [ -f ./hardware-configuration.nix ]; then
+    echo 'Found hardware-configration.nix. moving it to ./local/ ...'
+    mv ./hardware-configuration.nix ./local/
+  else
+    echo 'Generating hardware-configuration.nix...'
+    TMP_DIR="./tmp"
+    mkdir $TMP_DIR
+    nixos-generate-config --dir $TMP_DIR
+    mv $TMP_DIR/hardware-configuration.nix ./local/
+    rm $TMP_DIR/configuration.nix
+    rmdir $TMP_DIR
+  fi
 fi
+
 if [ ! -f ./local/optional.nix ]; then
   echo 'Copying optional.nix ...'
   cp samples/optional.nix ./local
