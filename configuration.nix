9a6f0b18115011ad1ae4c982498cd0a243655093
aster <137767097+aster-void@users.noreply.github.com>
2024-12-23 17:55:06 +0900

    run format check on CI

--- configuration.nix
+++ configuration.nix
@@ -2,8 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 # https://releases.nixos.org/nixos/unstable/
-{ ... }:
-{
+{ ... }: {
   imports =
     [
       # Include the results of the hardware scan.
