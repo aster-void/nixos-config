2ce3ab6b162f19b20620ebd2a94ae41a79b407b5
aster <137767097+aster-void@users.noreply.github.com>
2025-01-05 00:44:43 +0900

    simplify flake.nix and remove flake-utils from deps

--- flake.nix
+++ flake.nix
@@ -3,36 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = import nixpkgs { inherit system; }; in {
-          devShell = import ./shell.nix { inherit pkgs; };
-        })
-    //
-    (
-      let
-        extra = {
-          user = "aster";
-          git-email = "137767097+aster-void@users.noreply.github.com";
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      extra = {
+        user = "aster";
+        git-email = "137767097+aster-void@users.noreply.github.com";
+      };
+      mkSystemConfig = host: nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./nixos/configuration.nix
+          ./hosts/${host}
+        ];
+        extraArgs = extra // {
+          inherit host;
         };
-        systemConfig = host: nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./nixos/configuration.nix
-            ./hosts/${host}
-          ];
-          extraArgs = extra // {
-            inherit host;
-          };
-        };
-      in
-      {
-        nixosConfigurations.desktop = systemConfig "desktop";
-        nixosConfigurations.laptop = systemConfig "laptop";
-      }
-    );
+      };
+    in
+    {
+      devShell.${system} = import ./shell.nix { inherit pkgs; };
+      nixosConfigurations.desktop = mkSystemConfig "desktop";
+      nixosConfigurations.laptop = mkSystemConfig "laptop";
+    };
 }
