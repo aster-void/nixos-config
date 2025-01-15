3c386b9fdf21a28e7c0978170f2992a41fcd1125
aster <137767097+aster-void@users.noreply.github.com>
2025-01-15 22:15:07 +0900

    flatpak: add games (optional)

--- hosts/amberwood/default.nix
+++ hosts/amberwood/default.nix
@@ -8,6 +8,8 @@
     ../../modules/drivers/nvidia.nix
   ];
 
+  my.apps.games.enable = true;
+
   services.displayManager.autoLogin.user = user;
   services.displayManager.autoLogin.enable = false; # LY doesn't support
   boot.loader.grub.device = "nodev"; # or to use an actual device, use /dev/disk/by-uuid/YOUR_DEVICE_UUID_USE_ls_-lha_TO_CHECK
--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -1,29 +1,52 @@
 {
-  config,
   inputs,
+  lib,
+  config,
   ...
-}: {
+}: let
+  cfg = config.my.apps;
+in {
+  options.my.apps.games.enable = lib.mkEnableOption "Enable flatpak-installed games";
+
   imports = [inputs.nix-flatpak.nixosModules.nix-flatpak];
-
-  assertions = [
-    {
-      assertion = config.xdg.portal.enable;
-      message = "To use Flatpak you must enable XDG Desktop Portals with xdg.portal.enable.";
-    }
-  ];
-
-  services.flatpak = {
-    enable = true;
-    uninstallUnmanaged = true;
-    remotes = [
+  config = {
+    assertions = [
       {
-        name = "flathub";
-        location = "https://flathub.org/repo/flathub.flatpakrepo";
+        assertion = config.xdg.portal.enable;
+        message = "To use Flatpak you must enable XDG Desktop Portals with xdg.portal.enable.";
       }
     ];
-    packages = [
-      # { appId = "com.brave.Browser"; origin = "flathub"; }
-      "us.zoom.Zoom"
-    ];
+
+    services.flatpak = {
+      enable = true;
+      uninstallUnmanaged = true;
+      remotes = [
+        {
+          name = "flathub";
+          location = "https://flathub.org/repo/flathub.flatpakrepo";
+        }
+        {
+          name = "launcher.moe";
+          location = "https://gol.launcher.moe/gol.launcher.moe.flatpakrepo";
+        }
+      ];
+      packages =
+        [
+          # { appId = "com.brave.Browser"; origin = "flathub"; }
+          "us.zoom.Zoom"
+          "org.gnome.Platform//45"
+        ]
+        ++ lib.lists.optionals cfg.games.enable
+        [
+          {
+            appId = "moe.launcher.the-honkers-railway-launcher";
+            origin = "launcher.moe";
+          }
+          {
+            appId = "moe.launcher.sleepy-launcher";
+            origin = "launcher.moe";
+          }
+        ];
+    };
   };
 }
