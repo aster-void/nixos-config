16c890526947f28d58a8221d414fb7076526b957
aster <137767097+aster-void@users.noreply.github.com>
2025-01-21 15:52:00 +0900

    packages: install prismlauncher on bogster

--- hosts/bogster/default.nix
+++ hosts/bogster/default.nix
@@ -1,4 +1,8 @@
-{user, ...}: {
+{
+  user,
+  pkgs,
+  ...
+}: {
   imports = [
     ./hardware-configuration.nix
     ../../modules/desktop
@@ -9,4 +13,6 @@
   services.displayManager.autoLogin.user = user;
   services.displayManager.autoLogin.enable = false; # LY doesn't support auto login
   boot.loader.grub.device = "nodev"; # or to use an actual device, use /dev/disk/by-uuid/YOUR_DEVICE_UUID_USE_ls_-lha_TO_CHECK
+
+  environment.systemPackages = with pkgs; [prismlauncher];
 }
