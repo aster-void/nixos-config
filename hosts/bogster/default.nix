ab134f5bb7638ceed72d14ec9505ebe7c6ad5f11
aster <137767097+aster-void@users.noreply.github.com>
2025-01-23 18:04:04 +0900

    enable hardware.graphics for bogster'

--- hosts/bogster/default.nix
+++ hosts/bogster/default.nix
@@ -14,5 +14,8 @@
   services.displayManager.autoLogin.enable = false; # LY doesn't support auto login
   boot.loader.grub.device = "nodev"; # or to use an actual device, use /dev/disk/by-uuid/YOUR_DEVICE_UUID_USE_ls_-lha_TO_CHECK
 
+  hardware.graphics.enable = true;
+  hardware.graphics.extraPackages = [pkgs.intel-media-driver];
+
   environment.systemPackages = with pkgs; [prismlauncher];
 }
