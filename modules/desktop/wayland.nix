cc4eb01891bb231010a8aa207552b80fcaef23a9
aster <137767097+aster-void@users.noreply.github.com>
2025-04-01 15:31:14 +0900

    fix nvidia

--- modules/desktop/wayland.nix
+++ modules/desktop/wayland.nix
@@ -3,7 +3,7 @@
 
   environment.variables = {
     QT_QPA_PLATFORM = "wayland;xcb"; # allow fallback to x11 (xwayland) when wayland is not found
-    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto"; # hyprland wiki says it should be "auto"
   };
   environment.systemPackages = with pkgs; [wl-clipboard];
 }
