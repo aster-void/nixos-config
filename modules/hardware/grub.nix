a612b2c5b0a8b8931b8dbbcbeab23c10ad8c911a
aster <137767097+aster-void@users.noreply.github.com>
2025-04-09 19:50:20 +0900

    revert lanzaboote

--- modules/hardware/grub.nix
+++ modules/hardware/grub.nix
@@ -6,29 +6,31 @@
   imports = [
     inputs.lanzaboote.nixosModules.lanzaboote
   ];
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/var/lib/sbctl";
-  };
   environment.systemPackages = [pkgs.sbctl];
-  # Bootloader.
+  # enable secure boot with lanzaboote (todo)
+  # boot.lanzaboote = {
+  #   enable = true;
+  #   pkiBundle = "/var/lib/sbctl";
+  #   systemd-boot.enable = false;
+  # };
+  # grub.
   boot.loader = {
     systemd-boot = {
-      enable = false;
+      enable = true;
     };
     efi = {
       canTouchEfiVariables = true;
       efiSysMountPoint = "/boot";
     };
-    # grub = {
-    #   enable = false;
+    grub = {
+      enable = false;
 
-    #   efiSupport = true;
-    #   useOSProber = true;
-    #   # device = "nodev"; # ... example. define in hosts/{host}/default.nix
+      efiSupport = true;
+      useOSProber = true;
+      # device = "nodev"; # ... example. define in hosts/{host}/default.nix
 
-    #   default = "saved";
-    # };
-    # timeout = 10;
+      default = "saved";
+    };
+    timeout = 10;
   };
 }
