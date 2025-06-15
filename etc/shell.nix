7cfac2c570870b305ad54ab1647e6270cf9b1b52
System administrator <137767097+aster-void@users.noreply.github.com>
2025-06-15 13:59:58 +0900

    disable command-not-found

--- etc/shell.nix
+++ etc/shell.nix
@@ -3,11 +3,14 @@
 in {
   environment.shellAliases = shellAliases;
 
-  programs.bash = {
-    inherit shellAliases;
-    completion.enable = true;
-  };
-  programs.fish = {
-    inherit shellAliases;
+  programs = {
+    command-not-found.enable = false;
+    bash = {
+      inherit shellAliases;
+      completion.enable = true;
+    };
+    fish = {
+      inherit shellAliases;
+    };
   };
 }
