1ea84ab19d3bcf78aafb8d57229f1ed130ffb04f
aster <137767097+aster-void@users.noreply.github.com>
2025-08-23 03:03:28 +0900

    fix xkb options

--- etc/keymap.nix
+++ etc/keymap.nix
@@ -1,10 +1,10 @@
 {
   # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "workman";
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "workman";
 
-    xkbOptions = "caps:escape"; # CapsLock を Escape に
+    options = "caps:escape"; # CapsLock を Escape に
   };
 
   # make console (w/o x11 session) use the same keymap
